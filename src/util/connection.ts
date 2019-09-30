7d8664785232b1699007566857affa2a4058027d
brent <jinhoshin319@gmail.com>
2019-09-30 21:39:39 +0900

    removed key file

--- src/util/connection.ts
+++
@@ -1,7 +0,0 @@
-import { Firestore } from '@google-cloud/firestore';
-export const db: Firestore = new Firestore({
-	projectId: 'reactnativesampleserver',
-	keyFile: './ReactNativeSampleServer-b32381165a1b.json'
-});
-
-let a = 'a';
